73afa20ad73bd53b49f5cd756b2b1bcf29120137
Siddharth <75252554+sidcasmm@users.noreply.github.com>
2025-08-13 23:16:54 +0530

    Adds hashing function

--- lib/src/hls/hls_parser/hls_track_metadata_entry.dart
+++ lib/src/hls/hls_parser/hls_track_metadata_entry.dart
@@ -28,5 +28,11 @@ class HlsTrackMetadataEntry {
   }
 
   @override
-  int get hashCode => hashValues(groupId, name, variantInfos);
+  int get hashCode => Object.hash(
+        groupId,
+        name,
+        variantInfos != null
+            ? const ListEquality<VariantInfo>().hash(variantInfos!)
+            : null,
+      );
 }
